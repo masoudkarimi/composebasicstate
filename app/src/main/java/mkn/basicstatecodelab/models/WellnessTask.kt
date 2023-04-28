525b0e495329d9ea4a2c369e556547b910b38462
Masoud <karimi.masoud5494@gmail.com>
2023-04-28 17:09:08 +0330

    show list of tasks in WellnessScreen

---
+++ app/src/main/java/mkn/basicstatecodelab/models/WellnessTask.kt
@@ -0,0 +1,3 @@
+package mkn.basicstatecodelab.models
+
+data class WellnessTask(val id: Int, val label: String)
--- app/src/main/java/mkn/basicstatecodelab/screens/WellnessScreen.kt
+++ app/src/main/java/mkn/basicstatecodelab/screens/WellnessScreen.kt
@@ -1,10 +1,15 @@
 package mkn.basicstatecodelab.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import mkn.basicstatecodelab.components.StatefulCounter
+import mkn.basicstatecodelab.components.WellnessTasksList
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    StatefulCounter(modifier)
+    Column {
+        StatefulCounter(modifier)
+        WellnessTasksList()
+    }
 }
